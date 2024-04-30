db9d8e30b2c79000c350c2bb8879e0e3ec718309
Jeksos <165167845+Jeksos@users.noreply.github.com>
2024-04-30 17:40:20 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,12 +12,19 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 def est_authentifie():
     return session.get('authentifie')
 
+def est_authentifieUser():
+    return session.get('authentifieUser')
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
 
 @app.route('/fiche_nom/<string:post_nom>')
 def ReadficheNom(post_nom):
+    if not est_authentifieUser():
+        # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
+        return redirect(url_for('authentificationuser'))
+
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM clients WHERE nom = ?', (post_nom,))
@@ -40,6 +47,20 @@ def authentification():
     if request.method == 'POST':
         # Vérifier les identifiants
         if request.form['username'] == 'admin' and request.form['password'] == 'password': # password à cacher par la suite
+            session['authentifieUser'] = True
+            # Rediriger vers la route lecture après une authentification réussie
+            return redirect(url_for('lecture'))
+        else:
+            # Afficher un message d'erreur si les identifiants sont incorrects
+            return render_template('formulaire_authentification.html', error=True)
+
+    return render_template('formulaire_authentification.html', error=False)@app.route('/authentification', methods=['GET', 'POST'])
+    
+@app.route('/authentificationuser', methods=['GET', 'POST'])
+def authentificationUser():
+    if request.method == 'POST':
+        # Vérifier les identifiants
+        if request.form['username'] == 'user' and request.form['password'] == '12345': # password à cacher par la suite
             session['authentifie'] = True
             # Rediriger vers la route lecture après une authentification réussie
             return redirect(url_for('lecture'))
